de8ad3c9c6addd497f7d7feec13c6803733b4bb9
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-02-06 20:31:48 +0900

    플레이어 데미지 처리, 물리 처리

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,6 +9,10 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator animator;
+
+    private const int playerLayer = 10;
+    private const int plyaerDamagedLayer = 11;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,66 +23,106 @@ public class PlayerMove : MonoBehaviour
 
     // Update is called once per frame
     // 단발적인 일반적인 키 입력
-    void Update() {
+    void Update()
+    {
         //점프
-        if(Input.GetButtonDown("Jump") && !animator.GetBool("isJumping")) {
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("isJumping"))
+        {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             animator.SetBool("isJumping", true);
         }
-        
-        // 방향 전환
-        if(Input.GetButtonDown("Horizontal")) {
-            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-        }
 
         // 손 땟을때 자동으로 멈추게
-        if(Input.GetButtonUp("Horizontal")) {
+        if (Input.GetButtonUp("Horizontal"))
+        {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
 
         // 방향 전환
-        if(Input.GetButtonDown("Horizontal")) {
+        if (Input.GetButton("Horizontal"))
+        {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         }
 
-        if(Mathf.Abs(rigid.velocity.x) < 0.3) {
+        // 애니메이션
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+        {
             // 멈춘 상태
             animator.SetBool("isWalking", false);
-        } else {
+        }
+        else
+        {
             // 움직이고 있는 상태
             animator.SetBool("isWalking", true);
         }
-
-        
     }
 
     // FixedUpdate 는 물리계산같은게 들어갈때
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         // 오른쪽 왼쪽 움직임 제어
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-        if(rigid.velocity.x > maxSpeed) { 
+        if (rigid.velocity.x > maxSpeed)
+        {
             // 오른쪽 최대속력
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        } else if (rigid.velocity.x < maxSpeed * (-1)) {
+        }
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
             //왼쪽 최대속력
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
         }
 
         // Landing Platform
         // 하강시에만 Ray 발사
-        if(rigid.velocity.y < 0) {
-            Debug.DrawRay(rigid.position, Vector2.down, new Color(1,0,0)); 
-            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 1, LayerMask.GetMask("Platform"));
-            if(rayHit.collider != null) {
-            // Ray 는 Player 가운데서 나옴 , 플레이어 크기는 1
-                if(rayHit.distance < 0.5f) {
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector2.down, new Color(1, 0, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(
+                rigid.position,
+                Vector2.down,
+                1,
+                LayerMask.GetMask("Platform")
+            );
+            if (rayHit.collider != null)
+            {
+                // Ray 는 Player 가운데서 나옴 , 플레이어 크기는 1
+                if (rayHit.distance < 0.5f)
+                {
                     animator.SetBool("isJumping", false);
                 }
             }
         }
-        
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            OnDamaged(collision.transform.position);
+        }
+    }
 
+    void OnDamaged(Vector2 targetPosition)
+    {
+        gameObject.layer = plyaerDamagedLayer;
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        // 반작용 튕겨나감
+        int direction = transform.position.x - targetPosition.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(direction, 1) * 13, ForceMode2D.Impulse);
+
+        // 애니메이션
+        animator.SetTrigger("Damaged");
+
+        Invoke("OffDamaged", 3);
+    }
+
+    void OffDamaged()
+    {
+        // 레이어 바꿔서 무적 표현
+        gameObject.layer = playerLayer;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
 }
